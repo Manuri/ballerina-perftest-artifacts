1aa5a96bfd80eb950e1c0e40cc8b21fe034c87ae
Manuri <amaya.uom@gmail.com>
2018-07-20 05:55:16 +0000

    Update run-ballerina-test.sh

--- scripts/run-ballerina-test.sh
+++ scripts/run-ballerina-test.sh
@@ -4,4 +4,10 @@ bash ballerina-start.sh 1 data_service.bal
 
 sh ../jmeter/preconfigured_jmeter/apache-jmeter-4.0/bin/jmeter -n -t ../ballerina/jmx/ballerina-warmup.jmx
 
-sh ../jmeter/preconfigured_jmeter/apache-jmeter-4.0/bin/jmeter -t ../ballerina/jmx/ballerina.jmx -n  -e -l ../results/ballerina/jtls/$1.jtl -o ../results/ballerina/reports/$1 -Jthreads=$1 -Jduration=1200
+#sh ../jmeter/preconfigured_jmeter/apache-jmeter-4.0/bin/jmeter -t ../ballerina/jmx/ballerina.jmx -n  -e -l ../results/ballerina/jtls/$1.jtl -o ../results/ballerina/reports/$1 -Jthreads=$1 -Jduration=1200
+
+for var in 20 50 100 200 500 1000
+do
+sh ../jmeter/preconfigured_jmeter/apache-jmeter-4.0/bin/jmeter -t ../ballerina/jmx/ballerina.jmx -n  -e -l ../results/ballerina/jtls/$var.jtl -o ../results/ballerina/reports/$var -Jthreads=$var -Jduration=1200
+bash db-clean.sh
+done
